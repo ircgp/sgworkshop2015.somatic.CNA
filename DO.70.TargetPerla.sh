2fb1ba170fc1084cf3c5bc3883f09b1258a3ae54
kyoohyoungrho <kyoohyoungrho@gmail.com>
2015-07-03 15:47:56 +0900

    slight modification

--- DO.70.TargetPerla.sh
+++ DO.70.TargetPerla.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TargetPerla.pl SourceTarget.txt SS50M.bed SS50M
 
-# EXCAVATOR=/DATA/CUK/Somatic_Copy_Number_Alteration/EXCAVATOR_Package_v2.2/EXCAVATOR
-# rm -rf ${EXCAVATOR}/data
-# cp -R ${EXCAVATOR}/data EXCAVATOR_Package_v2.2/
+# EXCAVATOR=/DATA/CUK/Somatic_Copy_Number_Alteration/EXCAVATOR_Package_v2.2
+# rm -rf EXCAVATOR_Package_v2.2
+# cp -R ${EXCAVATOR} .
